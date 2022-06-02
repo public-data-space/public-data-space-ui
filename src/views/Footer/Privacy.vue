ea00a0869f2b338018eb8c9f1f934d4042e75b7e
Tulasch <claudia.tulasch@fokus.fraunhofer.de>
2022-06-02 15:26:43 +0200

    Add vue for imprint and privacy policy

---
+++ src/views/Footer/Privacy.vue
@@ -0,0 +1,218 @@
+<template>
+  <div>
+    <h1>Datenschutzerklärung</h1>
+    <h3>Datenschutzinformation Quartier der Zukunft - Open Data Portal</h3>
+    <p>
+      Im Rahmen der Nutzung des Quartier der Zukunft - Open Data Portals (Webauftritt und App) werden personenbezogene
+      Daten von Ihnen durch uns als den für die Datenverarbeitung Verantwortlichen
+      verarbeitet und für die Dauer gespeichert, die zur Erfüllung der festgelegten Zwecke und gesetzlicher
+      Verpflichtungen erforderlich ist. Im Folgenden informieren wir Sie darüber, um welche Daten
+      es sich dabei handelt, auf welche Weise sie verarbeitet werden und welche Rechte Ihnen diesbezüglich zustehen.
+
+      Personenbezogene Daten sind gemäß Art. 4 Nr. 1 Datenschutzgrundverordnung (DSGVO) alle Informationen, die sich auf
+      eine identifizierte oder identifizierbare natürliche Person beziehen.
+    </p>
+
+    <h3>Name und Kontaktdaten des für die Verarbeitung Verantwortlichen sowie des betrieblichen Datenschutzbeauftragten
+    </h3>
+    <p>Diese Datenschutzinformation gilt für die Datenverarbeitung unseres Angebots FOKUS Digital durch den
+      Verantwortlichen:</p>
+    <p>
+      Fraunhofer-Gesellschaft
+      <br>zur Förderung der angewandten Forschung e.V.
+      <br>Hansastraße 27 c,
+      <br>80686 München
+      <br>für das Fraunhofer Institut für Offene Kommunikationssysteme
+      <br>(im Folgenden „Fraunhofer-FOKUS“)
+      <br>E-Mail: info@fokus.fraunhofer.de
+      <br><br>
+      Der Datenschutzbeauftragte von Fraunhofer ist unter der o.g. Anschrift, zu Hd. Datenschutzbeauftragter bzw. unter
+      datenschutz@zv.fraunhofer.de erreichbar.
+      <br>
+      Sie können sich jederzeit bei Fragen zum Datenschutzrecht oder Ihren Betroffenenrechten direkt an unseren
+      Datenschutzbeauftragten wenden.
+
+    </p>
+
+
+    <h3>Verarbeitung personenbezogener Daten und Zwecke der Verarbeitung</h3>
+    <p>Wenn Sie das Quartier der Zukunft - Open Data Portal besuchen, speichern die Server unseres Dienstanbieters
+      temporär jeden Zugriff Ihres Endgerätes in einer Protokolldatei.
+      Folgende Daten werden erfasst und bis zur automatisierten Löschung gespeichert:</p>
+    <ul>
+      <li>IP-Adresse des anfragenden Rechners</li>
+      <li>Datum und Uhrzeit des Zugriffs</li>
+      <li>Name und URL der abgerufenen Daten</li>
+      <li>Übertragene Datenmenge</li>
+      <li>Meldung, ob der Abruf erfolgreich war</li>
+      <li>Verwendeter Browser und Browser-Einstellungen (Web-Applikation)</li>
+      <li>Betriebssystem</li>
+      <li>Webseite, von der aus der Zugriff erfolgt (Referrer-URL)</li>
+    </ul>
+
+    <p>
+      Wir verwenden diese Daten, um beispielsweise Funktionen zu aktivieren, Betrug zu verhindern, Sicherheitsmaßnahmen
+      zu implementieren, die Serververfügbarkeit sicherzustellen, App-Abstürze zu minimieren,
+      die Skalierbarkeit und Leistung zu verbessern oder die Kundenbetreuung durchzuführen.
+    </p>
+
+
+    <p>Rechtsgrundlagen für die vorstehenden Verarbeitungen sind</p>
+    <ul>
+      <li>für die Verarbeitung für den Besuch der App nach den Nummern 1-2 Art. 6 Abs. 1 S.1 lit. b DSGVO
+        (Erforderlichkeit für die Erfüllung des App-Nutzungsvertragsverhältnisses),</li>
+      <li>für die Verarbeitungen nach Nummer 3 Art. 6 Abs. 1 S.1 lit. c DSGVO (rechtliche Verpflichtung zur Umsetzung
+        technisch-organisatorischer Maßnahmen zur Sicherung der Datenverarbeitung nach Art. 32 DSGVO) und Art. 6 Abs. 1
+        S.1 lit. f DSGVO (berechtigte Interessen zur Datenverarbeitung für die Netz- und Informationssicherheit) sowie
+        für</li>
+      <li>die Verarbeitungen nach den Nummern 4-5 Art. 6 Abs. 1 S.1 lit. f DSGVO (berechtigte Interessen). Die
+        berechtigten Interessen unserer Datenverarbeitung bestehen darin, unser Angebot nutzerfreundlich zu gestalten
+        und zu optimieren.</li>
+      <li>Die vorstehend genannten Daten werden nach einer definierten Zeit vom Server automatisiert gelöscht. Sofern
+        Daten zu Zwecken nach Nummern 2-5 länger verarbeitet werden, erfolgt die Anonymisierung oder Löschung, wenn die
+        Speicherung für den jeweiligen Zweck nicht mehr erforderlich ist.</li>
+    </ul>
+
+
+    <h3>Weitergabe von personenbezogenen Daten</h3>
+    <p>
+      Außer in den zuvor genannten Fällen zur Nutzung der App, geben wir Ihre personenbezogenen Daten nur an Dritte,
+      d.h. andere natürliche oder juristische Personen
+      außer Ihnen (der betroffenen Person), dem Verantwortlichen oder dem Auftragsverarbeiter und deren zur
+      Datenverarbeitung befugten Mitarbeiter weiter, wenn:
+    </p>
+    <ul>
+      <li>für den Fall, dass für die Weitergabe nach Art. 6 Abs. 1 S. 1 lit. c DSGVO eine gesetzliche Verpflichtung
+        besteht, bspw. an Finanz- oder Strafverfolgungsbehörden;</li>
+      <li> die Weitergabe ist nach Art. 6 Abs. 1 S. 1 lit. f DSGVO zur Geltendmachung, Ausübung oder Verteidigung von
+        Rechtsansprüchen erforderlich und es besteht kein Grund zur Annahme, dass Sie ein überwiegendes schutzwürdiges
+        Interesse an der Nichtweitergabe Ihrer Daten haben; eine solche Weitergabe kann bspw. im Falle von Angriffen auf
+        unsere IT-Systeme an staatliche Einrichtungen und Strafverfolgungsbehörden erfolgen.</li>
+    </ul>
+
+    <p>
+      Die weitergegebenen Daten dürfen von dem Dritten ausschließlich zu den genannten Zwecken verwendet werden.
+    </p>
+    <p>
+      Im Übrigen wird unser FOKUS Digital gemäß Art. 28 DSGVO von unserem Vertragspartner Urban Software Institute GmbH,
+      Zwickauer Str. 223a, 09116 Chemnitz, Deutschland ausschließlich auf Servern der EU gehostet.
+    </p>
+    <p>
+      Eine Übermittlung von personenbezogenen Daten an ein Drittland (außerhalb der EU) oder eine internationale
+      Organisation ist ausgeschlossen.
+    </p>
+
+    <h3>Analyse/Tracking</h3>
+    <p>
+      Das Quartier der Zukunft - Open Data Portal setzt keine Cookies ein.
+    </p>
+
+    <h3>Eingesetzte Auftragsverarbeiter</h3>
+    <p>Folgende Organisationen, Unternehmen bzw. Personen wurden vom Betreiber dieser Website mit
+      der Verarbeitung von Daten beauftragt:</p>
+    <p>Urban Software Institute GmbH,
+      <br>Zwickauer Str. 223a,
+      <br>09116 Chemnitz, Germany
+    </p>
+
+    <h3>Betroffenenrechte</h3>
+    <p>Sie haben das Recht:</p>
+    <ul>
+      <li>gemäß Art. 7 Abs. 3 DSGVO Ihre einmal erteilte Einwilligung jederzeit gegenüber uns zu widerrufen. Dies hat
+        zur Folge, dass wir die Datenverarbeitung, die auf dieser Einwilligung beruhte, für die Zukunft nicht mehr
+        fortführen dürfen;</li>
+      <li>gemäß Art. 15 DSGVO Auskunft über Ihre von uns verarbeiteten personenbezogenen Daten zu verlangen.
+        Insbesondere können Sie Auskunft über die Verarbeitungszwecke, die Kategorie der personenbezogenen Daten, die
+        Kategorien von Empfängern, gegenüber denen Ihre Daten offengelegt wurden oder werden, die geplante
+        Speicherdauer, das Bestehen eines Rechts auf Berichtigung, Löschung, Einschränkung der Verarbeitung oder
+        Widerspruch, das Bestehen eines Beschwerderechts, die Herkunft ihrer Daten, sofern diese nicht bei uns erhoben
+        wurden, sowie über das Bestehen einer automatisierten Entscheidungsfindung einschließlich Profiling und ggf.
+        aussagekräftigen Informationen zu deren Einzelheiten verlangen;</li>
+      <li>gemäß Art. 16 DSGVO unverzüglich die Berichtigung unrichtiger oder Vervollständigung Ihrer bei uns
+        gespeicherten personenbezogenen Daten zu verlangen;</li>
+      <li>gemäß Art. 17 DSGVO die Löschung Ihrer bei uns gespeicherten personenbezogenen Daten zu verlangen, soweit
+        nicht die Verarbeitung zur Ausübung des Rechts auf freie Meinungsäußerung und Information, zur Erfüllung einer
+        rechtlichen Verpflichtung, aus Gründen des öffentlichen Interesses oder zur Geltendmachung, Ausübung oder
+        Verteidigung von Rechtsansprüchen erforderlich ist;</li>
+      <li>gemäß Art. 18 DSGVO die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen, soweit die
+        Richtigkeit der Daten von Ihnen bestritten wird, die Verarbeitung unrechtmäßig ist, Sie aber deren Löschung
+        ablehnen und wir die Daten nicht mehr benötigen, Sie jedoch diese zur Geltendmachung, Ausübung oder Verteidigung
+        von Rechtsansprüchen benötigen oder Sie gemäß Art. 21 DSGVO Widerspruch gegen die Verarbeitung eingelegt haben;
+      </li>
+      <li>gemäß Art. 20 DSGVO Ihre personenbezogenen Daten, die Sie uns bereitgestellt haben, in einem strukturierten,
+        gängigen und maschinenlesebaren Format zu erhalten oder die Übermittlung an einen anderen Verantwortlichen zu
+        verlangen und</li>
+      <li>gemäß Art. 77 DSGVO sich bei einer Aufsichtsbehörde zu beschweren. In der Regel können Sie sich hierfür an die
+        Aufsichtsbehörde ihres üblichen Aufenthaltsortes oder Arbeitsplatzes oder unseres Unternehmenssitzes wenden.
+      </li>
+    </ul>
+
+    <div class="alert alert-success" role="alert">
+      <p><strong>Information über Ihr Widerspruchsrecht nach Art. 21 DSGVO</strong></p>
+      <p>
+        Sie haben das Recht, aus Gründen, die sich aus Ihrer besonderen Situation ergeben, jederzeit gegen die
+        Verarbeitung Sie betreffender personenbezogener Daten, die aufgrund von Artikel 6 Absatz 1 Buchstabe e DSGVO
+        (Datenverarbeitung im öffentlichen Interesse) und Artikel 6 Absatz 1 Buchstabe f DSGVO (Datenverarbeitung auf
+        der Grundlage einer Interessenabwägung) erfolgt, Widerspruch einzulegen;
+        dies gilt auch für ein auf diese Bestimmung gestütztes Profiling von Artikel 4 Nr. 4 DSGVO.
+      </p>
+      <p>
+        Legen Sie Widerspruch ein, werden wir Ihre personenbezogenen Daten nicht mehr verarbeiten, es sei denn, wir
+        können zwingende schutzwürdige Gründe für die Verarbeitung nachweisen,
+        die Ihre Interessen, Rechte und Freiheiten überwiegen, oder die Verarbeitung dient der Geltendmachung, Ausübung
+        oder Verteidigung von Rechtsansprüchen.
+      </p>
+      <p>
+        Sofern sich Ihr Widerspruch gegen eine Verarbeitung von Daten zum Zwecke der Direktwerbung richtet, so werden
+        wir die Verarbeitung umgehend einstellen. In diesem Fall ist die Angabe einer besonderen Situation nicht
+        erforderlich.
+        Dies gilt auch für das Profiling, soweit es mit solcher Direktwerbung in Verbindung steht.
+      </p>
+      <p>
+        Möchten Sie von Ihrem Widerspruchsrecht Gebrauch machen, genügt eine E-Mail an datenschutz@zv.fraunhofer.de.
+
+      </p>
+    </div>
+
+    <h3>Datensicherheit</h3>
+    <p>
+      Der Backend-Server des Quartier der Zukunft - Open Data Portals sowie die Ausgaben selbst werden mit dem allgemein
+      üblichen und sicheren Standard TLS (Transport Layer Security) verschlüsselt übertragen. TLS ist ein sicherer und
+      erprobter Standard, der z.B. auch beim Onlinebanking Verwendung findet.
+      Sie erkennen eine sichere TLS-Verbindung unter anderem an dem angehängten s am http (also https://..) in der
+      Adressleiste Ihres Browsers oder am Schloss-Symbol im unteren Bereich Ihres Browsers.
+    </p>
+
+    <p>
+      Wir bedienen uns im Übrigen geeigneter technischer und organisatorischer Sicherheitsmaßnahmen, um Ihre Daten gegen
+      zufällige oder vorsätzliche Manipulationen, teilweisen oder vollständigen Verlust, Zerstörung oder gegen den
+      unbefugten Zugriff Dritter zu schützen.
+      Unsere Sicherheitsmaßnahmen werden entsprechend der technologischen Entwicklung fortlaufend verbessert.
+    </p>
+
+    <h3>Aktualität und Änderung dieser Datenschutzinformation</h3>
+    <p>
+      Diese Datenschutzinformation ist aktuell gültig.
+    </p>
+    <p>
+      Durch die Weiterentwicklung unseres Angebots oder aufgrund geänderter gesetzlicher bzw. behördlicher Vorgaben kann
+      es notwendig werden, diese Datenschutzinformation zu ändern.
+      Die jeweils aktuelle Datenschutzinformation kann jederzeit auf der Webseite unter Link auf diese Seite:
+      https://opendata.quartier-der-zukunft.de/privacypolicy von Ihnen abgerufen und ausgedruckt werden.
+    </p>
+
+    <h3>Salvatorische Klausel</h3>
+    <p>
+      Sollten einzelne Bestimmungen dieser Datenschutzerklärung ganz oder in Teilen unwirksam oder undurchführbar sein
+      oder werden, berührt dies nicht die Wirksamkeit der übrigen Bestimmungen. Entsprechendes gilt im Fall von Lücken.
+    </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Privacy',
+
+}
+</script>
